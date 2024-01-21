4f9ffd29195e8230fb2956ae019cea01d86e80a1
Pavel <opolpasa270@mail.ru>
2024-01-21 20:38:44 +0300

    add filters to search

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,15 +2,50 @@
 import axios from 'axios';
 import CardList from '../components/CardList.vue'
 import { ref, computed, reactive, onMounted, warn, watch } from "vue";
-  let search = ref('')
-  const wines = ref([])
 
+const wines = ref([])
 
+const filters = reactive({
+  search: '',
+  sugar: '',
+  color: '',
+
+})
+
+const onChangeSelectSugar = (event) => {
+  filters.sugar = event.target.value
+  console.log(filters.sugar)
+}
+const onChangeSelectColor = (event) => {
+  filters.color = event.target.value
+  console.log(filters.color)
+}
+const onChangeSearch = (event) => {
+  filters.search = event.target.value
+  console.log(filters.search)
+}
 
 const FetchWines = async () => {
-  try{
-    const {data} = await axios.get(
-      `https://e6c7f09dff4a57b0.mokky.dev/wines?title='*${search.value}*`
+  try {
+    const params = {
+      // title: '',
+      // sugar: filters.sugar,
+      // color: filters.color
+    }
+    if (filters.search) {
+      params.title = `*${filters.search}*`
+    }
+    if (filters.color) {
+      params.color = `${filters.color}`
+    }
+    if (filters.sugar) {
+      params.sugar = `${filters.sugar}`
+    }
+    const { data } = await axios.get(
+      `https://e6c7f09dff4a57b0.mokky.dev/wines`,
+      {
+        params
+      }
     )
 
     wines.value = data
@@ -24,32 +59,34 @@ onMounted(async () => {
   await FetchWines()
 })
 
-const onchangeSearch = (event) => {
-    search.value = event.target.value
-    console.log(search.value)
-  }
 
-watch(FetchWines)
+
+watch(
+  FetchWines,
+)
 
 </script>
 
 <template>
-      <h2 class="text-2xl">Товары</h2>
-    <div class="flex gap-4 items-center">
-      <input  @input="onchangeSearch" type="text" class="p-2 border-2 border-red-800 rounded-lg focus:outline-none " placeholder="Искать...">
-      <span class="text-slate-400">Сахар:</span>
-      <select class="p-2 border-2 border-red-800 rounded-lg focus:outline-none">
-        <option value="сладкое">сладкое</option>
-        <option value="полусладкое">полусладкое</option>
-        <option value="полусухое">полусухое</option>
-        <option value="сухое">сухое</option>
-      </select>
-      <span class="text-slate-400">Цвет:</span>
-      <select class="p-2 border-2 border-red-800 rounded-lg focus:outline-none">
-        <option value="красное">красное</option>
-        <option value="белое">белое</option>
-      </select>
-    </div>
-    
-    <CardList :wines="wines"/>
+  <h2 class="text-2xl">Товары</h2>
+  <div class="flex gap-4 items-center">
+    <input @input="onChangeSearch" type="text" class="p-2 border-2 border-red-800 rounded-lg focus:outline-none "
+      placeholder="Искать...">
+    <span class="text-slate-400">Сахар:</span>
+    <select @change="onChangeSelectSugar" class="p-2 border-2 border-red-800 rounded-lg focus:outline-none">
+      <option value="">все</option>
+      <option value="сладкое">сладкое</option>
+      <option value="полусладкое">полусладкое</option>
+      <option value="полусухое">полусухое</option>
+      <option value="сухое">сухое</option>
+    </select>
+    <span class="text-slate-400">Цвет:</span>
+    <select @change="onChangeSelectColor" class="p-2 border-2 border-red-800 rounded-lg focus:outline-none">
+      <option value="">все</option>
+      <option value="красное">красное</option>
+      <option value="белое">белое</option>
+    </select>
+  </div>
+
+  <CardList :wines="wines" />
 </template>
